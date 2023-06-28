51656864d7a8da51606efb271805c670c580f32c
jak-win <kuba358190000@gmail.com>
2023-06-29 00:38:11 +0200

    Commit końcowy - gotowa aplikacja

--- app/src/main/java/pl/wincenciuk/tipapp/MainActivity.kt
+++ app/src/main/java/pl/wincenciuk/tipapp/MainActivity.kt
@@ -9,9 +9,15 @@ import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Slider
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Adb
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Remove
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -23,8 +29,12 @@ import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import pl.wincenciuk.tipapp.components.InputField
 import pl.wincenciuk.tipapp.ui.theme.TipAppTheme
+import pl.wincenciuk.tipapp.util.calculateTotalPerPerson
+import pl.wincenciuk.tipapp.util.calculateTotalTip
+import pl.wincenciuk.tipapp.widgets.RoundIconButton
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -56,6 +66,7 @@ fun MyApp(content: @Composable () -> Unit) {
 fun TopHeader(totalPerPerson: Double = 134.0) {
     Surface(modifier = Modifier
         .fillMaxWidth()
+        .padding(15.dp)
         .height(150.dp)
         .clip(RoundedCornerShape(corner = CornerSize(12.dp))),
         color = Color(0xFFE9D7F7)
@@ -82,6 +93,41 @@ fun TopHeader(totalPerPerson: Double = 134.0) {
 @Preview
 @Composable
 fun MainContent() {
+
+    val splitByState = remember {
+        mutableStateOf(1)
+    }
+
+    val range = IntRange(start = 1, endInclusive = 100)
+
+    val tipAmountState = remember {
+        mutableStateOf(0.0)
+    }
+    val totalPerPersonState = remember {
+        mutableStateOf(0.0)
+    }
+
+    Column(modifier = Modifier.padding(all = 12.dp)) {
+        BillForm(splitByState = splitByState,
+        range = range,
+        tipAmountState = tipAmountState,
+        totalPerPersonState = totalPerPersonState)  {}
+
+    }
+
+}
+
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun BillForm(modifier: Modifier = Modifier,
+             range: IntRange = 1..100,
+             splitByState: MutableState<Int>,
+             tipAmountState: MutableState<Double>,
+             totalPerPersonState: MutableState<Double>,
+             onValChange: (String) -> Unit = {}
+            ){
+
     val totalBillState = remember {
         mutableStateOf("")
     }
@@ -90,15 +136,27 @@ fun MainContent() {
     }
     val keyboardController = LocalSoftwareKeyboardController.current
 
+    val sliderPositionState = remember {
+        mutableStateOf(0f)
+    }
+    val tipPercentage = (sliderPositionState.value * 100).toInt()
+
+
+
+    TopHeader(totalPerPerson = totalPerPersonState.value)
+
+
     Surface(
-        modifier = Modifier
+        modifier = modifier
             .padding(2.dp)
             .fillMaxWidth(),
         shape = RoundedCornerShape(corner = CornerSize(8.dp)),
         border = BorderStroke(width = 1.dp, color = Color.LightGray)
-            ) {
+    ) {
 
-        Column() {
+        Column(modifier = Modifier.padding(6.dp),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.Start) {
 
             InputField(valueState = totalBillState,
                 labelId = "Enter Bill",
@@ -106,9 +164,89 @@ fun MainContent() {
                 isSingleLine = true,
                 onAction = KeyboardActions{
                     if (!validState) return@KeyboardActions
+                    onValChange(totalBillState.value.trim())
+
                     keyboardController?.hide()
+                })
+            if (validState) {
+                Row(modifier = Modifier.padding(3.dp),
+                horizontalArrangement = Arrangement.Start) {
+                    Text(
+                        text = "Split",
+                        modifier = modifier.align(alignment = Alignment.CenterVertically)
+                    )
+                    Spacer(modifier = Modifier.width(120.dp))
+                    Row(
+                        modifier = Modifier.padding(horizontal = 3.dp),
+                        horizontalArrangement = Arrangement.End) {
+                        RoundIconButton(
+                            imageVector = Icons.Default.Remove,
+                            onClick = {
+                                splitByState.value =
+                                    if (splitByState.value > 1) splitByState.value - 1  else  1
+                                totalPerPersonState.value = calculateTotalPerPerson(totalBill = totalBillState.value.toDouble(),
+                                    splitBy = splitByState.value,
+                                    tipPercentage = tipPercentage)
+                            })
+                        
+                        Text(text = "${splitByState.value}",
+                            modifier = Modifier
+                                .align(Alignment.CenterVertically)
+                                .padding(start = 9.dp, end = 9.dp))
+
+                        RoundIconButton(
+                            imageVector = Icons.Default.Add,
+                            onClick = {
+                                if (splitByState.value < range.last) {
+                                    splitByState.value += 1
+                                    totalPerPersonState.value = calculateTotalPerPerson(totalBill = totalBillState.value.toDouble(),
+                                        splitBy = splitByState.value,
+                                        tipPercentage = tipPercentage)
+                                }
+
+                            })
+
+                    }
+
                 }
-            )
+            Row(modifier = Modifier
+                .padding(horizontal = 3.dp, vertical = 12.dp)) {
+                Text(text = "Tip",
+                    modifier = Modifier.align(alignment = Alignment.CenterVertically))
+                Spacer(modifier = Modifier.width(200.dp))
+                Text(
+                    text = "$ ${tipAmountState.value}",
+                    modifier = Modifier.align(alignment = Alignment.CenterVertically))
+
+            }
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(text = "$tipPercentage %", fontSize = 24.sp)
+
+                Spacer(modifier = Modifier.height(14.dp))
+
+                Slider(value = sliderPositionState.value,
+                    onValueChange = { newVal ->
+                        sliderPositionState.value = newVal
+                        tipAmountState.value = calculateTotalTip(totalBill = totalBillState.value.toDouble(),
+                            tipPercentage = tipPercentage)
+
+                        totalPerPersonState.value = calculateTotalPerPerson(totalBill = totalBillState.value.toDouble(),
+                        splitBy = splitByState.value,
+                        tipPercentage = tipPercentage)
+                    },
+                    modifier = Modifier.padding(start = 16.dp, end = 16.dp),
+
+                    onValueChangeFinished = {
+
+                    })
+            }
+
+          } else {
+                Box() {}
+            }
         }
 
     }
@@ -117,12 +255,12 @@ fun MainContent() {
 
 
 
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     TipAppTheme {
         MyApp {
-            Text(text = "Hello Again!")
 
         }
 
